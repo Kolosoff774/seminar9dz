dd7add17c000f69c78452925f3f18139e46a7174
Andrey <akolosov48@gmail.com>
2023-09-16 11:51:53 +0500

    добавлены программы по итоговой аттестации

---
+++ zadacha2/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу,
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+Выполнить с помощью рекурсии.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+Console.Write("Введите натуральное число: ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите натуральное число: ");
+int number2 = int.Parse(Console.ReadLine());
+if (number1 > 0 && number2 > 0) 
+{
+    Console.WriteLine(SumNaturalNumbers(number1, number2));
+} else Console.Write("Введите положительное натуральное число!");
+
+
+int SumNaturalNumbers(int num1, int num2) 
+{
+    int sum = 1;
+    if (num1 < num2) 
+    {
+        sum = num1 + SumNaturalNumbers(num1 + 1, num2);
+    }
+    else if(num1 > num2) 
+    {
+        sum = num2 + SumNaturalNumbers(num1, num2 + 1);
+    }
+    else sum = num1;
+    return sum;
+}
---
+++ zadacha3/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+*/
+
+Console.Write("Введи неотрицательное число m: ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введи неотрицательное число n: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 >= 0 && number2 >= 0) 
+{
+    Console.WriteLine(Ack(number1, number2));
+} else Console.Write("Введите положительное число!");
+
+int Ack(int m, int n) 
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return Ack(m - 1, 1);
+    return Ack(m - 1, Ack(m, n - 1));
+}
